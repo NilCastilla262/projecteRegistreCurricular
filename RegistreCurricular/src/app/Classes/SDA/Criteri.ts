66951f6fc6f1017a13296406dbf13dd756c3c094
Noureddin El Adila <noureddineladila@gmail.com>
2024-10-18 17:30:54 +0200

    correct criteri error + create personal classes

--- RegistreCurricular/src/app/Classes/SDA/Criteri.ts
+++ RegistreCurricular/src/app/Classes/SDA/Criteri.ts
@@ -1,35 +1,31 @@
 export class Criteri {
-    private _UUIdCriteriValue : string;
-    private _UUIdCompetenciaDescripcioPlantilla : string;
-    private _UUIdCriteriPlantilla : string;
-    private _treballat : string ;
-    constructor() {
-        this._UUIdCriteriValue ="";
-        this._treballat  ="";
-        this._UUIdCompetenciaDescripcioPlantilla= "";
-        this._UUIdCriteriPlantilla = "";
-    }
-
+  private _UUIdCriteriValue: string;
+  private _UUIdCompetenciaDescripcioPlantilla: string;
+  private _UUIdCriteriPlantilla: string;
+  private _treballat: string;
+  constructor() {
+    this._UUIdCriteriValue = '';
+    this._treballat = '';
+    this._UUIdCompetenciaDescripcioPlantilla = '';
+    this._UUIdCriteriPlantilla = '';
+  }
 }
 
-
 export class Criteries {
-    criteris: Criteri[] = [];
+  private _criteris: Criteri[];
+  constructor() {
+    this._criteris = [];
+  }
 }
 
 export class CriteriPlantilla {
-    private _UUIdCriteriPlantilla : string;
-    private _UUIdCompetenciaDescripcioPlantilla : string;
-    private _descripcio : string;
-    
+  private _UUIdCriteriPlantilla: string;
+  private _UUIdCompetenciaDescripcioPlantilla: string;
+  private _descripcio: string;
 
-    constructor() {
-        this._UUIdCriteriPlantilla ="";
-        this._UUIdCompetenciaDescripcioPlantilla = "";
-        this._descripcio = "";
-        }
-
-
-
-
+  constructor() {
+    this._UUIdCriteriPlantilla = '';
+    this._UUIdCompetenciaDescripcioPlantilla = '';
+    this._descripcio = '';
+  }
 }
