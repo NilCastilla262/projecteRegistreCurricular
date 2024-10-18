fe44a3e6a5df211ee91fb183fead42194f24fc2d
storrent391 <storrent391@boscdelacoma.cat>
2024-10-18 17:39:06 +0200

    Added Class center

---
+++ RegistreCurricular/src/app/Classes/Personal/Center.ts
@@ -0,0 +1,12 @@
+export class Center {
+    private _UUIdCenter: string;
+    private _UUIdUser: string;
+    private _LogoUrl: string;
+    private _CenterName: string;
+    constructor() {
+        this._UUIdCenter = "";
+        this._UUIdUser = "";
+        this._LogoUrl = "";
+        this._CenterName = "";
+    }
+}
