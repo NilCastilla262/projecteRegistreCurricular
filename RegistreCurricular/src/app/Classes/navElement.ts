1e0c16d0a52a2b59d9bea52e35009877fd9d4fa4
Noureddin El Adila <noureddineladila@gmail.com>
2024-11-18 17:08:30 +0100

    Last commit , before creating the new Project

---
+++ RegistreCurricular/src/app/Classes/navElement.ts
@@ -0,0 +1,10 @@
+export class navElement {
+  elementNumber: number;
+  userNum: number;
+  description: string;
+  constructor() {
+    this.elementNumber = 0;
+    this.userNum = 0;
+    this.description = '';
+  }
+}
--- RegistreCurricular/src/app/Components/menu/menu.component.ts
+++ RegistreCurricular/src/app/Components/menu/menu.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import { navElement } from '../../Classes/navElement';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
 })
 export class MenuComponent {
+  userNumber: number = 2;
 
+  navBar: navElement[] = [
+    {
+      elementNumber: 0,
+      userNum: 0,
+      description: 'Gestion Center sAdmin',
+    },
+
+    {
+      elementNumber: 1,
+      userNum: 0,
+      description: 'Edita Plantilla sAdmin',
+    },
+
+    {
+      elementNumber: 2,
+      userNum: 0,
+      description: 'Editar Usuaris sAdmin',
+    },
+
+    {
+      elementNumber: 3,
+      userNum: 1,
+      description: 'gestionar usuaris  Admin',
+    },
+
+    {
+      elementNumber: 4,
+      userNum: 1,
+      description: 'generar sdas Admin ',
+    },
+
+    {
+      elementNumber: 5,
+      userNum: 1,
+      description: 'gestionar resums Admin ',
+    },
+
+    {
+      elementNumber: 6,
+      userNum: 2,
+      description: 'Gestion Sda Profe',
+    },
+
+    {
+      elementNumber: 7,
+      userNum: 2,
+      description: 'Gestion Resum profe',
+    },
+  ];
 }
