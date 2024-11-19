373241b49fd1756a732d9eff8047734eed0029b7
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2024-11-19 15:56:11 +0100

    Created test 'should navigate to manage-sda when the first button is clicked'

--- NewCurricular/src/components/Professor/p-main/p-main.component.spec.ts
+++ NewCurricular/src/components/Professor/p-main/p-main.component.spec.ts
@@ -1,23 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router, RouterModule } from '@angular/router';
 import { PMainComponent } from './p-main.component';
+import { By } from '@angular/platform-browser';
 
 describe('PMainComponent', () => {
   let component: PMainComponent;
   let fixture: ComponentFixture<PMainComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PMainComponent]
-    })
-    .compileComponents();
+      imports: [
+        RouterModule.forRoot([
+          { path: 'manage-sda', component: PMainComponent }
+        ]),
+        PMainComponent
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PMainComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to manage-sda when the first button is clicked', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+
+    const button = fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click', null);
+
+    expect(navigateSpy).toHaveBeenCalledWith(['/manage-sda']);
   });
 });
