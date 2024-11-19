fea0017d280ef751ce08466ab9f1dc66a997148d
Noureddin El Adila <noureddineladila@gmail.com>
2024-11-19 20:06:46 +0100

    Use the getallCompetencyDescriptionPl service function on the ts to show the list of descriptions on the html

--- NewCurricular/src/components/show-sda/show-sda.component.ts
+++ NewCurricular/src/components/show-sda/show-sda.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CompetencyService } from '../../services/competency.service';
 
 @Component({
   selector: 'app-show-sda',
   standalone: true,
   imports: [],
   templateUrl: './show-sda.component.html',
-  styleUrl: './show-sda.component.css'
+  styleUrl: './show-sda.component.css',
 })
 export class ShowSdaComponent {
+  competencyDescriptionList: any[] = [];
+  competencyNamesList: any[] = [];
+  competencyTypesList: any[] = [];
+  CriteriesList: any[] = [];
+  competencyService = inject(CompetencyService);
 
+  ngOnInit(): void {
+    this.getAllCompetencyDescriptions();
+  }
+
+  getAllCompetencyDescriptions() {
+    this.competencyService.getAllCompetencyDescription().subscribe(
+      (data: any[]) => {
+        this.competencyDescriptionList = data; // Assign data to the local variable
+      },
+      (error) => {
+        console.error('Error fetching competencies:', error); // Handle errors
+      }
+    );
+  }
 }
