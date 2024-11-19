b37bb45faad7cd78eae73eb059fecc9b4395e645
Noureddin El Adila <noureddineladila@gmail.com>
2024-11-19 16:23:12 +0100

    create the queries needed to get all the getALL for cometency

---
+++ Backend/models/competencyQueries.js
@@ -0,0 +1,86 @@
+// models/competencyQueries.js
+const { poolPromise } = require("../config/db");
+
+async function getAllCompetencyTypesPl() {
+  try {
+    const pool = await poolPromise;
+    const result = await pool
+      .request()
+      .query("SELECT * FROM CompetencyType_Pl");
+    return result.recordset;
+  } catch (error) {
+    console.error("Query failed:", error.message);
+    throw error;
+  }
+}
+async function getAllCompetencyNamesPl() {
+  try {
+    const pool = await poolPromise;
+    const result = await pool
+      .request()
+      .query("SELECT * FROM CompetencyName_Pl");
+    return result.recordset;
+  } catch (error) {
+    console.error("Query failed:", error.message);
+    throw error;
+  }
+}
+async function getAllCompetencyDescriptionsPl() {
+  try {
+    const pool = await poolPromise;
+    const result = await pool
+      .request()
+      .query("SELECT * FROM CompetencyDescription_Pl");
+    return result.recordset;
+  } catch (error) {
+    console.error("Query failed:", error.message);
+    throw error;
+  }
+}
+
+async function getAllCompetencyTypesVal() {
+  try {
+    const pool = await poolPromise;
+    const result = await pool
+      .request()
+      .query("SELECT * FROM CompetencyType_Val");
+    return result.recordset;
+  } catch (error) {
+    console.error("Query failed:", error.message);
+    throw error;
+  }
+}
+async function getAllCompetencyNamesVal() {
+  try {
+    const pool = await poolPromise;
+    const result = await pool
+      .request()
+      .query("SELECT * FROM CompetencyName_Val");
+    return result.recordset;
+  } catch (error) {
+    console.error("Query failed:", error.message);
+    throw error;
+  }
+}
+async function getAllCompetencyDescriptionsVal() {
+  try {
+    const pool = await poolPromise;
+    const result = await pool
+      .request()
+      .query("SELECT * FROM CompetencyDescription_Val");
+    return result.recordset;
+  } catch (error) {
+    console.error("Query failed:", error.message);
+    throw error;
+  }
+}
+
+module.exports = {
+  getAllCompetencyTypesPl,
+  getAllCompetencyNamesPl,
+  getAllCompetencyDescriptionsPl,
+  getAllCompetencyTypesVal,
+  getAllCompetencyNamesVal,
+  getAllCompetencyDescriptionsVal,
+};
+ 
