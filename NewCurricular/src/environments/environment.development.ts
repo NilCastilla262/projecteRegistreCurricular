2934ef5b1e23d1813ff420e317846a018f1715ba
Noureddin El Adila <noureddineladila@gmail.com>
2024-11-19 19:55:10 +0100

    Create the endPoints

--- NewCurricular/src/environments/environment.development.ts
+++ NewCurricular/src/environments/environment.development.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  api_main_url: 'http://localhost:3000/api/',
+  api_url_Competency: 'http://localhost:3000/api/Competency/',
+  api_url_Criteria: 'http://localhost:3000/api/Criteria/',
+  api_url_Curs: 'http://localhost:3000/api/Curs/',
+  api_url_User: 'http://localhost:3000/api/User/',
+  api_url_GroupTable: 'http://localhost:3000/api/GroupTable/',
+  api_url_Plantilla: 'http://localhost:3000/api/Plantilla/',
+  api_url_Vectors: 'http://localhost:3000/api/Vectors/',
 };
