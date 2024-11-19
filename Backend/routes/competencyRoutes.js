4def0da1275ab7ea13c652116cd4f378064aa449
Noureddin El Adila <noureddineladila@gmail.com>
2024-11-19 15:55:28 +0100

    add competency Route map

---
+++ Backend/routes/competencyRoutes.js
@@ -0,0 +1,19 @@
+// routes/competencyRoutes.js
+const express = require("express");
+const router = express.Router();
+const competencyController = require("../controllers/competencyController");
+
+router.get("/competencyTypePl", competencyController.getAllCompetencyTypesPl);
+router.get("/competencyNamePl", competencyController.getAllCompetencyNamesPl);
+router.get(
+  "/competencyDescriptionPl",
+  competencyController.getAllCompetencyDescriptionsPl
+);
+router.get("/competencyTypeVal", competencyController.getAllCompetencyTypesVal);
+router.get("/competencyNameVal", competencyController.getAllCompetencyNamesVal);
+router.get(
+  "/competencyDescriptionVal",
+  competencyController.getAllCompetencyDescriptionsVal
+);
+
+module.exports = router;
