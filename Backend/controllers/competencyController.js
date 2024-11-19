55a288534f94f3cc749de6af66fb9744ef8bb0f8
Noureddin El Adila <noureddineladila@gmail.com>
2024-11-19 16:17:31 +0100

    Create the Competency controller to manage all the getALL queries needed from module

---
+++ Backend/controllers/competencyController.js
@@ -0,0 +1,67 @@
+// controllers/competencyController.js
+const competencyQueries = require("../models/competencyQueries");
+
+async function getAllCompetencyTypesPl(req, res) {
+  try {
+    const competencies = await competencyQueries.getAllCompetencyTypesPl();
+    res.json(competencies);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve Competency Types Pl" });
+  }
+}
+async function getAllCompetencyDescriptionsPl(req, res) {
+  try {
+    const competencies =
+      await competencyQueries.getAllCompetencyDescriptionsPl();
+    res.json(competencies);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to retrieve competency Description Pl" });
+  }
+}
+async function getAllCompetencyNamesPl(req, res) {
+  try {
+    const competencies = await competencyQueries.getAllCompetencyNamesPl();
+    res.json(competencies);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve Competency Name Pl" });
+  }
+}
+
+async function getAllCompetencyTypesVal(req, res) {
+  try {
+    const competencies = await competencyQueries.getAllCompetencyTypesVal();
+    res.json(competencies);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve Competency Type val " });
+  }
+}
+async function getAllCompetencyDescriptionsVal(req, res) {
+  try {
+    const competencies =
+      await competencyQueries.getAllCompetencyDescriptionsVal();
+    res.json(competencies);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to retrieve competency Description val " });
+  }
+}
+async function getAllCompetencyNamesVal(req, res) {
+  try {
+    const competencies = await competencyQueries.getAllCompetencyNamesVal();
+    res.json(competencies);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve Competency Val " });
+  }
+}
+
+module.exports = {
+  getAllCompetencyTypesPl,
+  getAllCompetencyNamesPl,
+  getAllCompetencyDescriptionsPl,
+  getAllCompetencyTypesVal,
+  getAllCompetencyNamesVal,
+  getAllCompetencyDescriptionsVal,
+};
