bb8e5e2264f80812d9668f8172af1bab5b7eeeaf
storrent391 <storrent391@boscdelacoma.cat>
2024-11-20 20:24:18 +0100

    Solved errors on the ng model that made login compoonent to not to work

--- NewCurricular/src/components/login/login.component.ts
+++ NewCurricular/src/components/login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
