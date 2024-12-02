73d26627d69240b926c01d32bcb6f384ea9adfe2
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2024-12-02 18:21:33 +0100

    Created the html with the minimum necessary in the component create center form

--- NewCurricular/src/components/SuperAdmin/create-center-form/create-center-form.component.ts
+++ NewCurricular/src/components/SuperAdmin/create-center-form/create-center-form.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-center-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-center-form.component.html',
   styleUrl: './create-center-form.component.css'
 })
 export class CreateCenterFormComponent {
+  center = {
+    name: '',
+    adminEmail: '',
+    municipality: ''
+  };
 
+  centerCreated?: boolean;
+
+  municipalities: string[] = [
+    'Argelaguer',
+    'Besalú',
+    'Beuda',
+    'Castellfollit de la Roca',
+    'Les Planes d’Hostoles',
+    'Maià de Montcal',
+    'Mieres',
+    'Montagut i Oix',
+    'Olot',
+    'Riudaura',
+    'Sant Aniol de Finestres',
+    'Sant Feliu de Pallerols',
+    'Sant Ferriol',
+    'Santa Pau',
+    'Tortellà',
+    'Vall de Bianya',
+    'Vall d’en Bas'
+  ];
+
+  createCenter(): void {
+  }
+
+  isFormValid(): boolean {
+    return true;
+  }
 }
