c6ddcd5163b8dd227f98c767a0f2b5764809a5fd
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2024-12-03 15:30:16 +0100

    Created test should toggle button enabled state based on form validity

--- NewCurricular/src/components/SuperAdmin/create-center-form/create-center-form.component.spec.ts
+++ NewCurricular/src/components/SuperAdmin/create-center-form/create-center-form.component.spec.ts
@@ -45,4 +45,38 @@ describe('CreateCenterFormComponent', () => {
 
     expect(button.disabled).toBeFalse();
   });
+
+  it('should toggle button enabled state based on form validity', () => {
+    const button = fixture.nativeElement.querySelector('button');
+    const inputName = fixture.nativeElement.querySelector('input#centerName');
+    const inputEmail = fixture.nativeElement.querySelector('input#adminEmail');
+    const selectMunicipality = fixture.nativeElement.querySelector('select#municipality');
+  
+    expect(button.disabled).toBeTrue();
+  
+    inputName.value = 'Nom Prova';
+    inputName.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+  
+    expect(button.disabled).toBeTrue();
+  
+    inputEmail.value = 'admin@prova.com';
+    inputEmail.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+  
+    expect(button.disabled).toBeTrue();
+  
+    selectMunicipality.value = component.municipalities[0];
+    selectMunicipality.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+  
+    expect(button.disabled).toBeFalse();
+  
+    inputName.value = '';
+    inputName.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+  
+    expect(button.disabled).toBeTrue();
+  });
+  
 });
