96e453a9cd465aae6565f84fc98470f87baf32a4
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2024-12-03 15:24:16 +0100

    Refactor test should disable the button if no data in any field of the form

--- NewCurricular/src/components/SuperAdmin/create-center-form/create-center-form.component.spec.ts
+++ NewCurricular/src/components/SuperAdmin/create-center-form/create-center-form.component.spec.ts
@@ -24,31 +24,25 @@ describe('CreateCenterFormComponent', () => {
     expect(button).toBeTruthy();
     expect(button.disabled).toBeTrue();
 
-    // Omplim el camp "Nom del Centre"
     const inputName = fixture.nativeElement.querySelector('input#centerName');
     inputName.value = 'Nom Prova';
     inputName.dispatchEvent(new Event('input'));
     fixture.detectChanges();
 
-    // Encara hauria d'estar deshabilitat perquè falten camps
     expect(button.disabled).toBeTrue();
 
-    // Omplim el camp "Correu Electrònic de l'Administrador"
     const inputEmail = fixture.nativeElement.querySelector('input#adminEmail');
     inputEmail.value = 'admin@prova.com';
     inputEmail.dispatchEvent(new Event('input'));
     fixture.detectChanges();
 
-    // Encara hauria d'estar deshabilitat perquè falta el municipi
     expect(button.disabled).toBeTrue();
 
-    // Seleccionem un municipi
     const selectMunicipality = fixture.nativeElement.querySelector('select#municipality');
     selectMunicipality.value = component.municipalities[0];
     selectMunicipality.dispatchEvent(new Event('change'));
     fixture.detectChanges();
 
-    // Ara el botó hauria d'estar habilitat
     expect(button.disabled).toBeFalse();
   });
 });
