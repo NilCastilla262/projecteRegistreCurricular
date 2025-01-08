6e89bf46cd5dbb4a98767648d15c10d658b1b09d
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2025-01-08 17:37:47 +0100

    Last uncommited changes 2024

---
+++ NewCurricular/src/Constants/Center.ts
@@ -0,0 +1,14 @@
+export class center{
+    
+  uuid_center: string;
+  logoUrl: string;
+  municipality: string;
+  nomCentre: string;
+
+  constructor(){
+    this.uuid_center = '';
+    this.logoUrl = '';
+    this.municipality = '';
+    this.nomCentre = '';
+  }
+}
