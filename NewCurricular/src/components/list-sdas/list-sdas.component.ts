88f7e8dc4b8cf7afbf604dd0f32ef90f18293d75
Noureddin El Adila <noureddineladila@gmail.com>
2025-01-15 17:35:52 +0100

    Create List-sdas

---
+++ NewCurricular/src/components/list-sdas/list-sdas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-sdas',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-sdas.component.html',
+  styleUrl: './list-sdas.component.css'
+})
+export class ListSdasComponent {
+
+}
